f1f5b888fac39d3d91e5613ab35f0faa91fa711e
Alvin Cherk <alvin@cherk.org>
2023-02-27 13:53:59 +1100

    Redo envs again

--- frontend/util/firebase.ts
+++ frontend/util/firebase.ts
@@ -1,26 +1,14 @@
 import { init } from "next-firebase-auth";
 
-const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
-const CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL;
-const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY;
+const FIREBASE_PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 const FIREBASE_DATABASE_URL = process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL;
-const COOKIE_SECRET_CURRENT = process.env.COOKIE_SECRET_CURRENT;
-const COOKIE_SECRET_PREVIOUS = process.env.COOKIE_SECRET_PREVIOUS;
 
-if (PROJECT_ID === undefined) {
+if (FIREBASE_PROJECT_ID === undefined) {
   throw new Error("Missing NEXT_PUBLIC_FIREBASE_PROJECT_ID");
-}
-
-if (CLIENT_EMAIL === undefined) {
-  throw new Error("Missing FIREBASE_CLIENT_EMAIL");
 }
 
 if (FIREBASE_DATABASE_URL === undefined) {
   throw new Error("Missing NEXT_PUBLIC_FIREBASE_DATABASE_URL");
-}
-
-if (COOKIE_SECRET_CURRENT === undefined || COOKIE_SECRET_PREVIOUS === undefined) {
-  throw new Error("Firebase cookie values not set");
 }
 
 const initAuth = () => {
@@ -39,11 +27,13 @@ const initAuth = () => {
     // firebaseAuthEmulatorHost: "localhost:9099",
     firebaseAdminInitConfig: {
       credential: {
-        projectId: PROJECT_ID,
-        clientEmail: CLIENT_EMAIL,
+        projectId: FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? "",
         // The private key must not be accessible on the client side.
         privateKey:
-          FIREBASE_PRIVATE_KEY !== undefined ? JSON.parse(FIREBASE_PRIVATE_KEY) : undefined,
+          process.env.FIREBASE_PRIVATE_KEY !== undefined
+            ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+            : undefined,
       },
       databaseURL: FIREBASE_DATABASE_URL,
     },
@@ -62,7 +52,7 @@ const initAuth = () => {
       name: "capstone390023t1-githappens", // required
       // Keys are required unless you set `signed` to `false`.
       // The keys cannot be accessible on the client side.
-      keys: [COOKIE_SECRET_CURRENT, COOKIE_SECRET_PREVIOUS],
+      keys: [process.env.COOKIE_SECRET_CURRENT, process.env.COOKIE_SECRET_PREVIOUS],
       httpOnly: true,
       maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
       overwrite: true,
