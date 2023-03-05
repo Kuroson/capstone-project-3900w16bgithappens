95ad4ced0b5b779317bb6765f8560266f5f196d7
Alvin Cherk <alvin@cherk.org>
2023-03-05 23:32:14 +1100

    Rearrange login

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import { Button, TextField } from "@mui/material";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { AuthAction, useAuthUser, withAuthUser } from "next-firebase-auth";
-import { SideNavbar } from "components";
+import { ContentContainer, SideNavbar } from "components";
 
 const LoginPage = (): JSX.Element => {
   const [email, setEmail] = React.useState("");
@@ -12,14 +12,25 @@ const LoginPage = (): JSX.Element => {
 
   const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Don't reload page
-    console.log(email, password);
     await signInWithEmailAndPassword(getAuth(), email, password)
       .then((user) => {
         console.log("Signed in successfully", user);
       })
       .catch((err) => {
-        console.error(err.message);
-        toast.error(err.message);
+        // See error codes:
+        // https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#signinwithemailandpassword
+        if (err?.code === "auth/invalid-email") {
+          toast.error("Invalid email");
+        } else if (err?.code === "auth/user-disabled") {
+          toast.error("User disabled");
+        } else if (err?.code === "auth/user-not-found") {
+          toast.error("User not found");
+        } else if (err?.code === "auth/wrong-password") {
+          toast.error("Wrong password");
+        } else {
+          console.error(err);
+          toast.error("Error Uncaught");
+        }
       });
   };
 
@@ -31,30 +42,44 @@ const LoginPage = (): JSX.Element => {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <SideNavbar empty={true} />
-      <div className="w-full flex flex-col px-[5%]">
-        <h1 className="text-center pt-4 text-4xl">Login Page</h1>
-        <form className="flex flex-col justify justify-center py-10" onSubmit={handleOnSubmit}>
-          <TextField
-            id="email-input"
-            label="Email"
-            type="text"
-            variant="outlined"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button variant="contained" type="submit" id="loginSubmissionButton">
-            Login
-          </Button>
+      <ContentContainer>
+        <form
+          className="w-full h-full flex flex-col items-center pt-[6rem]"
+          onSubmit={handleOnSubmit}
+        >
+          <h1 className="text-6xl">Welcome to GitHappens!</h1>
+          <div className="flex flex-col justify-between mt-10 w-[25rem]">
+            <div>
+              <TextField
+                id="email-input"
+                label="Email"
+                type="text"
+                variant="outlined"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-[25rem]"
+                autoComplete="email"
+              />
+            </div>
+            <div className="pt-4">
+              <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-[25rem]"
+                autoComplete="new-password"
+              />
+            </div>
+            <div className="w-[25rem] mt-4">
+              <Button variant="contained" className="w-[25rem]" type="submit">
+                Login
+              </Button>
+            </div>
+          </div>
         </form>
-      </div>
+      </ContentContainer>
     </>
   );
 };
