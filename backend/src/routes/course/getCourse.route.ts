e1398dff5eace970cca8cb89653ab8ce2b17b5cc
Alvin Cherk <alvin@cherk.org>
2023-03-18 16:40:55 +1100

    Change route to only allow request of admins or enrolled students

--- backend/src/routes/course/getCourse.route.ts
+++ backend/src/routes/course/getCourse.route.ts
@@ -1,6 +1,7 @@
 import { HttpException } from "@/exceptions/HttpException";
 import Course, { CourseInterface } from "@/models/course.model";
 import { PageInterface } from "@/models/page.model";
+import User from "@/models/user.model";
 import { checkAuth } from "@/utils/firebase";
 import { logger } from "@/utils/logger";
 import { ErrorResponsePayload, getMissingBodyIDs, isValidBody } from "@/utils/util";
@@ -16,6 +17,15 @@ type QueryPayload = {
     courseCode: string;
 };
 
+/**
+ * GET /course
+ * Gets the course's information. Must be enrolled in said course or be an admin
+ *
+ * NOTE: untested atm
+ * @param req
+ * @param res
+ * @returns
+ */
 export const getCourseController = async (
     req: Request<QueryPayload>,
     res: Response<ResponsePayload | ErrorResponsePayload>,
@@ -26,7 +36,7 @@ export const getCourseController = async (
         if (isValidBody<QueryPayload>(req.query, KEYS_TO_CHECK)) {
             const { courseCode } = req.query;
 
-            const courseData = await getCourse(courseCode);
+            const courseData = await getCourse(courseCode, authUser.uid);
 
             return res.status(200).json({ ...courseData });
         } else {
@@ -52,13 +62,25 @@ export const getCourseController = async (
  * it contains
  *
  * @param courseId The ID of the course to be recalled
+ * @param firebaseUID The firebaseUID of the user requesting the course
  * @returns Base information on the course based on return requirements in ResponsePayload
  */
-export const getCourse = async (courseId: string): Promise<CourseInformation> => {
+export const getCourse = async (
+    courseId: string,
+    firebaseUID: string,
+): Promise<CourseInformation> => {
+    // Find user first
+    const user = await User.findOne({ firebase_uid: firebaseUID }).catch(() => null);
+    if (user === null) throw new HttpException(400, `User of ${firebaseUID} does not exist`);
+
+    // Check if user is enrolled in course
     const myCourse = await Course.findById(courseId)
         .select("_id title code description session icon pages")
         .populate("pages");
 
     if (myCourse === null) throw new HttpException(400, `Course of ${courseId} does not exist`);
+    if (!myCourse.students.includes(user._id))
+        throw new HttpException(400, "User is not enrolled in course");
+
     return myCourse;
 };
