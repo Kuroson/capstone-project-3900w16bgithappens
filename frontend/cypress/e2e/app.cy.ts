58dfbab3dda06058eadb0e67e330fa601bc774d6
Alvin Cherk <alvin@cherk.org>
2023-03-31 15:17:04 +1100

    Deprecate test

--- frontend/cypress/e2e/app.cy.ts
+++ frontend/cypress/e2e/app.cy.ts
@@ -16,16 +16,16 @@ describe("Login tests", () => {
 
   // NOTE: This test is kinda bad, assumes that this account already exists.
   // Should remove this
-  it("Login", () => {
-    cy.location("pathname").should("eq", "/login");
-    cy.get("#email-input").focus().type("cypressloginaccount@cypress.com");
-    cy.get("#outlined-password-input").focus().type("Password123!"); // TODO: convert to env variable maybe
-    cy.get("#submit-form-button").click();
-    cy.wait(250);
-    cy.get(".Toastify > div").should("not.exist"); // Error
-    cy.location("pathname").should("eq", "/");
-    cy.get("h1").contains("Welcome, Cypress Account");
-  });
+  // it("Login", () => {
+  //   cy.location("pathname").should("eq", "/login");
+  //   cy.get("#email-input").focus().type("cypressloginaccount@cypress.com");
+  //   cy.get("#outlined-password-input").focus().type("Password123!"); // TODO: convert to env variable maybe
+  //   cy.get("#submit-form-button").click();
+  //   cy.wait(250);
+  //   cy.get(".Toastify > div").should("not.exist"); // Error
+  //   cy.location("pathname").should("eq", "/");
+  //   cy.get("h1").contains("Welcome, Cypress Account");
+  // });
 });
 
 // Prevent TypeScript from reading file as legacy script
