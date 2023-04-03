8534775ea12be530a040b5cc503c87c7ac4dc22e
Alvin Cherk <alvin@cherk.org>
2023-04-03 11:27:50 +1000

    Extract component into new file

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,6 +1,7 @@
 import { AdminNavBar, ContentContainer, EmptyNavBar, Layout, StudentNavBar } from "./Layout";
 import NoUserLayout from "./Layout/NoUserLayout";
 import EditPanelButtons from "./admin/EditPanelButtons";
+import EditOnlineClassSection from "./admin/onlineClass/EditOnlineClassSection";
 import AddNewResourceSection from "./admin/resource/AddNewResourceSection";
 import ResourcesSection from "./admin/resource/ResourcesSection";
 import SingleEditableResource from "./admin/resource/SingleEditableResource";
@@ -24,4 +25,5 @@ export {
   Loading,
   NoUserLayout,
   OnlineClassCard,
+  EditOnlineClassSection,
 };
--- frontend/pages/instructor/[courseId]/onlineClass/[onlineClass].tsx
+++ frontend/pages/instructor/[courseId]/onlineClass/[onlineClass].tsx
@@ -4,8 +4,6 @@ import YouTube from "react-youtube";
 import Head from "next/head";
 import { LoadingButton } from "@mui/lab";
 import { Button, TextField } from "@mui/material";
-import { DateTimePicker } from "@mui/x-date-pickers";
-import { set } from "cypress/types/lodash";
 import { UserCourseInformation } from "models/course.model";
 import { MessageInterface } from "models/message.model";
 import { OnlineClassFull } from "models/onlineClass.model";
@@ -13,17 +11,11 @@ import { UserDetails } from "models/user.model";
 import moment from "moment";
 import { GetServerSideProps } from "next";
 import { AuthAction, useAuthUser, withAuthUser, withAuthUserTokenSSR } from "next-firebase-auth";
-import { AdminNavBar, ContentContainer, Loading } from "components";
+import { AdminNavBar, ContentContainer, EditOnlineClassSection, Loading } from "components";
 import { HttpException } from "util/HttpExceptions";
 import { useUser } from "util/UserContext";
 import { getUserCourseDetails } from "util/api/courseApi";
-import {
-  UpdateOnlineClassPayloadRequest,
-  endOnlineClass,
-  sendOnlineClassMessage,
-  startOnlineClass,
-} from "util/api/onlineClassApi";
-import { updateOnlineClass } from "util/api/onlineClassApi";
+import { endOnlineClass, sendOnlineClassMessage, startOnlineClass } from "util/api/onlineClassApi";
 import { getOnlineClassDetails } from "util/api/onlineClassApi";
 import initAuth from "util/firebase";
 import { youtubeURLParser } from "util/util";
@@ -33,123 +25,6 @@ initAuth(); // SSR maybe, i think...
 type OnlineClassPageProps = {
   courseData: UserCourseInformation;
   onlineClassData: OnlineClassFull;
-};
-
-type EditOnlineClassProps = {
-  dynamicOnlineClass: OnlineClassFull;
-  setDynamicOnlineClass: React.Dispatch<React.SetStateAction<OnlineClassFull>>;
-  setEditMode: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-const EditOnlineClass = ({
-  dynamicOnlineClass,
-  setDynamicOnlineClass,
-  setEditMode,
-}: EditOnlineClassProps): JSX.Element => {
-  const authUser = useAuthUser();
-
-  const [loading, setLoading] = React.useState(false);
-  const [title, setTitle] = React.useState(dynamicOnlineClass.title);
-  const [description, setDescription] = React.useState(dynamicOnlineClass.description);
-  const [linkToClass, setLinkToClass] = React.useState(dynamicOnlineClass.linkToClass);
-  const [startTime, setStartTime] = React.useState(dynamicOnlineClass.startTime);
-
-  const handleOnSubmit = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    if (title === "") {
-      toast.warning("Please enter a title");
-      return;
-    }
-    if (startTime === null) {
-      toast.warning("Please enter a start time");
-      return;
-    }
-    if (linkToClass === "" || youtubeURLParser(linkToClass) === false) {
-      toast.warning("Please enter a valid YouTube URL link");
-      return;
-    }
-    setLoading(true);
-    const newOnlineClassData: UpdateOnlineClassPayloadRequest = {
-      title,
-      description,
-      linkToClass,
-      startTime,
-      classId: dynamicOnlineClass._id,
-    };
-
-    const [res, err] = await updateOnlineClass(
-      await authUser.getIdToken(),
-      newOnlineClassData,
-      "client",
-    );
-
-    if (err !== null) {
-      console.error(err);
-      if (err instanceof HttpException) {
-        toast.error(err.message);
-      } else {
-        toast.error(err);
-      }
-      setLoading(false);
-      return;
-    }
-    if (res === null) throw new Error("Res should not have been null");
-    setDynamicOnlineClass({ ...dynamicOnlineClass, ...newOnlineClassData });
-    setLoading(false);
-    toast.success("Successfully updated online class");
-    setEditMode(false);
-  };
-
-  return (
-    <form className="w-full" onSubmit={handleOnSubmit}>
-      <div>
-        <TextField
-          id="Title"
-          label="Course Title"
-          variant="outlined"
-          onChange={(e) => setTitle(e.target.value)}
-          className="w-full"
-          value={title}
-        />
-      </div>
-      <div className="pt-5">
-        <DateTimePicker
-          label="Date Time"
-          className="w-full"
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          onChange={(e: any) => setStartTime(e.unix())}
-          value={moment.unix(startTime)}
-        />
-      </div>
-      <div className="pt-5">
-        <TextField
-          id="Link"
-          label="URL Link"
-          variant="outlined"
-          onChange={(e) => setLinkToClass(e.target.value)}
-          className="w-full"
-          value={linkToClass}
-        />
-      </div>
-      <div className="pt-5">
-        <TextField
-          id="Description"
-          label="Description"
-          variant="outlined"
-          multiline
-          rows={9}
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-          className="w-full"
-        />
-      </div>
-      <div className="pt-5 w-full">
-        <LoadingButton variant="contained" className="w-full" loading={loading} type="submit">
-          Submit Changes
-        </LoadingButton>
-      </div>
-    </form>
-  );
 };
 
 type LeftColumnProps = {
@@ -230,7 +105,7 @@ const LeftColumn = ({
   return (
     <div className="w-full flex flex-col justify-center items-center px-[5%]">
       {editMode ? (
-        <EditOnlineClass
+        <EditOnlineClassSection
           dynamicOnlineClass={dynamicOnlineClass}
           setDynamicOnlineClass={setDynamicOnlineClass}
           setEditMode={setEditMode}
