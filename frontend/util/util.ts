4d7a1f9b690079b7fc413768e166ae9faf01a031
Alvin Cherk <alvin@cherk.org>
2023-04-06 11:59:31 +1000

    Fix typo

--- frontend/util/util.ts
+++ frontend/util/util.ts
@@ -69,5 +69,7 @@ export const fileToDataUrl = (file: File) => {
  */
 export const adminRouteAccess = async (token: string | null, email: string): Promise<boolean> => {
   const [res, err] = await getUserDetails(token, email ?? "", "ssr");
-  return res?.userDetails === null || res?.userDetails.role !== 0;
+  console.log(res);
+  console.log(res?.userDetails.role !== 0);
+  return res !== null && res.userDetails !== null && res.userDetails.role === 0;
 };
